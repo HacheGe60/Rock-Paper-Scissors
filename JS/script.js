77eb6550b296cb6cfd948adeaf8231fa14fc4987
HacheGe60 <horaciodev1706@gmail.com>
2023-03-05 23:14:27 -0300

    Add 5 rounds

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,13 @@
-'use strict'
+"use strict";
 
 let capitalize = (str) => {
-    str = str.toLowerCase();
-    let strFirst = str.slice(0, 1);
-    str = str.slice(1, str.length);
-    strFirst = strFirst.toUpperCase();
-    str = strFirst + str;
-    return str;
-  };
+  str = str.toLowerCase();
+  let strFirst = str.slice(0, 1);
+  str = str.slice(1, str.length);
+  strFirst = strFirst.toUpperCase();
+  str = strFirst + str;
+  return str;
+};
 
 function getComputerChoice() {
   switch (Math.floor(Math.random() * 3) + 1) {
@@ -22,21 +22,38 @@ function getComputerChoice() {
   }
 }
 
-function whoWins() {
-  let computerSelection = getComputerChoice();
-  let playerSelection = prompt("What do you choose? Rock, Paper or Scissors");
-  playerSelection = capitalize(playerSelection);
-  console.log(playerSelection);
-  
-  if (playerSelection !== 'Rock' && playerSelection !== 'Paper' && playerSelection !== 'Scissors') {
-    return `You Lose! You didn't choose Rock, Paper or Scissors`;
-  } else if ((computerSelection === 'Rock' && playerSelection === 'Scissors') || (computerSelection === 'Paper' && playerSelection === 'Rock') || (computerSelection === 'Scissors' && playerSelection === 'Paper')) {
-    return `You Lose! ${computerSelection} beats ${playerSelection}`;
-  } else if ((playerSelection === 'Rock' && computerSelection === 'Scissors') || (playerSelection === 'Paper' && computerSelection === 'Rock') || (playerSelection === 'Scissors' && computerSelection === 'Paper')) {
-    return `Computer Lose! ${playerSelection} beats ${computerSelection}`;
-  } else {
-    return 'Draw!!!'
-  }
-}
+for (let i = 0; i < 5; i++) {
+  function playRound() {
+    let computerSelection = getComputerChoice();
+    let playerSelection = prompt("What do you choose? Rock, Paper or Scissors");
 
-document.write(whoWins());
+    if (playerSelection === null) {
+        return `You Lose! You didn't choose Rock, Paper or Scissors<br><br>`;
+    }
+
+    playerSelection = capitalize(playerSelection);
+
+    if (
+      playerSelection !== "Rock" &&
+      playerSelection !== "Paper" &&
+      playerSelection !== "Scissors"
+    ) {
+      return `You Lose! You didn't choose Rock, Paper or Scissors<br><br>`;
+    } else if (
+      (computerSelection === "Rock" && playerSelection === "Scissors") ||
+      (computerSelection === "Paper" && playerSelection === "Rock") ||
+      (computerSelection === "Scissors" && playerSelection === "Paper")
+    ) {
+      return `You Lose! ${computerSelection} beats ${playerSelection}<br><br>`;
+    } else if (
+      (playerSelection === "Rock" && computerSelection === "Scissors") ||
+      (playerSelection === "Paper" && computerSelection === "Rock") ||
+      (playerSelection === "Scissors" && computerSelection === "Paper")
+    ) {
+      return `Computer Lose! ${playerSelection} beats ${computerSelection}<br><br>`;
+    } else {
+      return "Draw!!!<br><br>";
+    }
+  }
+  document.write(playRound());
+}
